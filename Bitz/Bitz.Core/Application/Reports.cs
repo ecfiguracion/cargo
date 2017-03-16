c7dbcadc90149f2e274e61d385ba3042dc30b338
winux <alwinbombita@gmail.com>
2017-03-16 15:33:48 +0800

    Report RPT0006 Added and RPT0003 updated

--- Bitz/Bitz.Core/Application/Reports.cs
+++ Bitz/Bitz.Core/Application/Reports.cs
@@ -34,14 +34,16 @@ namespace Bitz.Core.Application
       public static CoreConstants.Report RPT0003 { get { return FindById(1102); } }
       public static CoreConstants.Report RPT0004 { get { return FindById(1103); } }
       public static CoreConstants.Report RPT0005 { get { return FindById(1104); } }
+      public static CoreConstants.Report RPT0006 { get { return FindById(1105); } }
 
       public static void Initialise()
       {
         Items.Add(new CoreConstants.Report(1100, "RPT0001", "Statement of Account", "Print statement of accounts.", "RPT0001.DLL"));
         Items.Add(new CoreConstants.Report(1101, "RPT0002", "Statement of Account - Roro", "Print RORO statement of accounts.", "RPT0002.DLL"));
-        Items.Add(new CoreConstants.Report(1102, "RPT0003", "PPA Due - Summary of Gross Revenue", "Print Summary of Gross Revenue by date range", "RPT0003.DLL", UserInterfaces.Reports.RPT0003View));
+        Items.Add(new CoreConstants.Report(1102, "RPT0003", "PPA Due - Schedule of Gross Revenue", "Print Schedule of Gross Revenue by date range", "RPT0003.DLL", UserInterfaces.Reports.RPT0003View));
         Items.Add(new CoreConstants.Report(1103, "RPT0004", "Statement of Account - Mooring/Unmooring", "Print Mooring/Unmooring statement of accounts.", "RPT0004.DLL"));
         Items.Add(new CoreConstants.Report(1104, "RPT0005", "Statement of Account - Porterage", "Print Porterage statement of accounts.", "RPT0005.DLL"));
+        Items.Add(new CoreConstants.Report(1105, "RPT0006", "PPA Due - Summary of Gross Revenue", "Print Summary of Gross Revenue by date range", "RPT0006.DLL", UserInterfaces.Reports.RPT0006View));
       }
     }
 
--- Bitz/Bitz.Core/Application/UserInterface.cs
+++ Bitz/Bitz.Core/Application/UserInterface.cs
@@ -67,11 +67,14 @@ namespace Bitz.Core.Application
     public static class Reports
     {
       public static CoreConstants.UserInterface RPT0003View { get { return FindById(1300); } }
+      public static CoreConstants.UserInterface RPT0006View { get { return FindById(1301); } }
 
       public static void Initialise()
       {
-        Items.Add(new CoreConstants.UserInterface(1300, "RPT-0003 Summary of Gross Revenue", "Bitz.Reports.Views.RPT0003View", Modules.Reports, CoreConstants.PageType.Dialog));
+        Items.Add(new CoreConstants.UserInterface(1300, "RPT-0003 Schedule of Gross Revenue", "Bitz.Reports.Views.RPT0003View", Modules.Reports, CoreConstants.PageType.Dialog));
+        Items.Add(new CoreConstants.UserInterface(1301, "RPT-0006 Summary of Gross Revenue", "Bitz.Reports.Views.RPT0006View", Modules.Reports, CoreConstants.PageType.Dialog));
       }
+
     }
 
     #endregion
